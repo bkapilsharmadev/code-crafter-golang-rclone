4609c95c74677140ae691c324638791d90bf8a20
Kiran Sabne <kiransabne04@gmail.com>
2024-07-26 13:51:52 +0530

    pass stage 9 expiry implemented in both set & get command

--- app/command.go
+++ app/command.go
@@ -183,7 +183,7 @@ func GetCommand(c net.Conn, args []string) string {
 	if len(args) != 1 {
 		return "-ERR wrong number of arguments for 'GET' command\r\n"
 	}
-	key := strings.TrimSpace(args[0])
+	key := args[0]
 	val, prsnt := SetStore[key]
 	if !prsnt {
 		return "$-1\r\n"
