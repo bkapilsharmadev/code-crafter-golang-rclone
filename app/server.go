262eb70bff3908ebed00da1f0b566a1fae67147c
Kiran Sabne <kiransabne04@gmail.com>
2024-07-26 11:15:41 +0530

    pass stage 5 - responding to multiple pings from same connection

--- app/server.go
+++ app/server.go
@@ -15,14 +15,24 @@ func main() {
 		fmt.Println("Failed to binf port 6379")
 		os.Exit(1)
 	}
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accpeting new connections ", err.Error())
+			os.Exit(1)
+		}
 
-	c, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accpeting new connections ", err.Error())
-		os.Exit(1)
+		handleConn(c)
 	}
 
 	// *1\r\n$4\r\nPING\r\n
-	c.Write([]byte("+PONG\r\n"))
+	//c.Write([]byte("+PONG\r\n"))
+}
 
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+
+	for {
+		conn.Write([]byte("+PONG\r\n"))
+	}
 }
